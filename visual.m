ff97a68e5b4e4e025db030cc6df4eb33d3debc44
ZhengchaoW <chaoswanzc@gmail.com>
2020-04-11 22:43:14 -0400

    Add files via upload

--- visual.m
+++ visual.m
@@ -1,32 +1,32 @@
-function d = visual(i,j)
+function [d,d1] = visual(i,j)
     %dm is the slb matrix
     load Us.mat;
     Xi = Us{i};
-    nbi = Xi.nb
-    UXi = Xi.ux;
     ui = Xi.ux;
+    nxi = length(ui);
     Xj = Us{j};
-    nbj = Xj.nb
-    UXj = Xj.ux;
     uj = Xj.ux;
+    nxj = length(uj);
     
     d = ugwslb(ui,uj,1);
     d1 = dgwslb(ui,uj);
     
-    lnkxi = linkage(squareform(UXi));
-    subplot(2,2,1), dendrogram(lnkxi); title(['Xi ugw-slb=' num2str(d)])
+    lnkxi = linkage(squareform(ui));
+    subplot(2,2,1), dendrogram(lnkxi); title(['Xi ugw-slb=' num2str(d)]);
+    axis([0 nxi+1 0 0.6])
     %axis([0 ny 0 diam])
 
 
 
-    lnkxj = linkage(squareform(UXj));
+    lnkxj = linkage(squareform(uj));
     subplot(2,2,2), dendrogram(lnkxj); title(['Xj dgw-slb=' num2str(d1) ]);
+    axis([0 nxj+1 0 0.6])
     %axis([0 nx 0 diam])
     
     subplot(2,2,3), 
-    hi = histogram(squareform(UXi),'Normalization','probability');
+    hi = histogram(squareform(ui),'Normalization','probability');
     hi.NumBins = 100;
     subplot(2,2,4), 
-    hj = histogram(squareform(UXj),'Normalization','probability');
+    hj = histogram(squareform(uj),'Normalization','probability');
     hj.NumBins = 100;
 end
