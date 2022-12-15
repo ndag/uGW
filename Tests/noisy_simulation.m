934d51e04d92e21f43b765756710f43de2d8cec3
Christoph Weitkamp <cweika@mathematik.uni-goettingen.de>
2022-12-15 19:02:19 +0100

    Small update.

--- Tests/noisy_simulation.m
+++ Tests/noisy_simulation.m
@@ -4,43 +4,63 @@ load Us.mat
 L = length(Us);
 scale = [0 0.2 0.4 0.6];%Noise levels
 S = length(scale);
-    Yi = Us{35};
-    uyi = Yi.ux;
-    nyi = length(uyi);  
-    muyi= ones(1,length(uyi))/length(uyi);
+
+
+DistUs = cell(L,S);
+
+% Preparation of the disturbed spaces
+for k = 1:S
+     for i=1:L
+        Y.uy = disturb_ultrametric_space(Us{i}.ux, scale(k));
+        Y.muy =  ones(1,length(Us{i}.ux))/length(Us{i}.ux);
+        DistUs{i,k} = Y;
+     end
+
+end
+
+save DisturbedUs.mat DistUs
+
 
 %parameters for uGWapprox
 p= 1;
-iterations= 10;
+iterations= 100;
 num_samples=5;
 num_skips=20;
-parpool(4)
+%parpool(4)
+
+ uSLB_res = cell(S);
+ dSLB_res = cell(S);
+ uTLB_res = cell(S);
+ dTLB_res = cell(S);
+ dGW_res  = cell(S);
+ uGW_res  = cell(S);
+
 
 for k = 1:S
     k
     uSLB= zeros(L,L);
     dSLB = zeros(L,L);
+    uTLB= zeros(L,L);
+    dTLB = zeros(L,L);
     dGW = zeros(L,L);
     uGW = zeros(L,L);
-    lab = zeros(1,L); % labels
 
 
     for i=1:L
-        Xi = Us{i};
-        ui = Xi.ux;
-        lab(i) = Xi.nb; % use number of blocks as label
-        mui= ones(1,length(ui))/length(ui);
-        ui = disturb_ultrametric_space(ui, scale(k));
         i
-        for j=i+1:L
-            Xj = Us{j};
-            uj = Xj.ux;
-            muj= ones(1,length(uj))/length(uj);
-            uj = disturb_ultrametric_space(uj, scale(k));
+        ui  = DistUs{i,k}.uy;
+        mui = DistUs{i,k}.muy;
+
+        for j=i:L
+
+            uj  = DistUs{j,k}.uy;
+            muj = DistUs{j,k}.muy;
             % calculate the lower bound/GW-dist etc.
             uSLB(i,j)= ugwslb(ui,uj,mui,muj,p);
+            uTLB(i,j)= ugwtlb(ui,uj,mui,muj,p);
             uGW(i,j)= ultraGWcgd(ui,uj,mui,muj,p,iterations,num_samples, num_skips);
             dSLB(i,j)= dgwslb(ui,uj,mui,muj,p);
+            dTLB(i,j)= dgwtlb(ui,uj,mui,muj,p);
             dGW(i,j)= dGWcgd(ui,uj,mui,muj,p,iterations,num_samples, num_skips);
         end
     end
@@ -49,19 +69,19 @@ for k = 1:S
     uGW = max(uGW,uGW');
     dSLB= max(dSLB,dSLB');
     dGW = max(dGW,dGW');
-    delete(gcp('nocreate'))
+    dTLB = max(dTLB,dTLB');
+    uTLB = max(uTLB,uTLB');
 
-    noise = scale(k);
+    %delete(gcp('nocreate'))
     
-    [sl il] = sort(lab);
-    subplot(4,S,k,'align'), imagesc(uSLB(il,il)); title(['uSLB perturbation at level ',num2str(noise)])
-    axis square
-    subplot(4,S,S+k,'align'), imagesc(uGW(il,il)); title(['uGW perturbation at level ',num2str(noise)])
-    axis square
-    subplot(4,S,2*S+k,'align'), imagesc(dSLB(il,il)); title(['dSLB perturbation at level ',num2str(noise)])
-    axis square
-    subplot(4,S,3*S+k,'align'), imagesc(dGW(il,il)); title(['dGW perturbation at level ',num2str(noise)])
-    axis square
+ uSLB_res{k} = uSLB;
+ dSLB_res{k} = dSLB;
+ uTLB_res{k} = uTLB;
+ dTLB_res{k} = dTLB;
+ dGW_res{k}  = dGW;
+ uGW_res{k}  = uGW;
 
 end
+
+save('uGWApprox','uSLB_res','dSLB_res', 'uTLB_res','dTLB_res', 'uGW_res','dGW_res')
 
